57dd846fa473f4b56bbdf25d11cfe1a0427deb7a
Taha711 <72921177+Taha711@users.noreply.github.com>
2022-09-07 18:11:10 +0200

    Commit

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'blue',
-    text: 'Hello world',
+    text: 'Hello world a',
     alignItems: 'center',
     justifyContent: 'center',
   },
